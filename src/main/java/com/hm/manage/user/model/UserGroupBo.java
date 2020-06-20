4fe42fa6b5a0aba96abbde71ea5c6e321333e0f9
e <1425178814@qq.com>
2020-06-21 00:02:32 +0800

    UserGroup添加UserInfo面向对象编程

--- src/main/java/com/hm/manage/user/model/UserGroupBo.java
+++ src/main/java/com/hm/manage/user/model/UserGroupBo.java
@@ -1,9 +1,11 @@
 package com.hm.manage.user.model;
 
+import com.hm.serve.userInfo.model.UserInfo;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * @创建人 【郝苗】
@@ -15,4 +17,5 @@ import java.io.Serializable;
 public class UserGroupBo implements Serializable {
     private Long id;// 用户组表
     private String userGroupName;// 用户组名
+    private List<UserInfo> children;// 用户信息
 }
